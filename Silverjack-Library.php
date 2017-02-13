3fce4fa77f1b6ed0280f80684917d98e8d021a2a
Ben Holland <ben.holland.core@gmail.com>
2017-02-13 09:32:24 -0800

    Added Requirements forr Silverjack-Library.Run()

--- Silverjack-Library.php
+++ Silverjack-Library.php
@@ -1,3 +1,11 @@
 function Run(){
-
+    /*Return an object that can be displayed by the display routines.
+    *Should contain:
+    *Each Player
+    *   Their Name
+    *   Their Hand
+    *      Each Card
+    *   Whether they won or lost
+    *   (Optional) The sum of their hand.
+    */
 };
