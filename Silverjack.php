0fe9a2e67e82ff6972e50615f7e2855a8acb5e2f
Ben Holland <bholland@csumb.edu>
2017-02-13 17:49:12 +0000

    Added Silverjack Landing Page

---
+++ Silverjack.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Silverjack
+        </title>
+        <<style type="text/css">
+            @import "root.css";
+        </style>
+        <?PHP
+        ?>
+    </head>
+    <body>
+        <div id="silverjack_Board">
+            <!--Silverjack Display Stuff Here-->
+        </div>
+        <div>
+            <div id="Play_Again_Button">
+               <!--Button should just refresh page-->         
+            </div>
+        </div>
+    </body>
+</html>
