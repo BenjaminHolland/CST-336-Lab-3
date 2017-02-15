0168b21487b9c1462e09816a439a581530955af7
Ben Holland <bholland@csumb.edu>
2017-02-15 20:46:52 +0000

    Basic Functionality Complete

--- Silverjack-Display.php
+++ Silverjack-Display.php
@@ -1,37 +1,65 @@
 <?PHP
     include 'Silverjack-Library.php';
     function adapt($rawState){
+       // var_dump($rawState);
         return $rawState;
-        /*
-        $state=array(
-            'players'=>array(
-                array(
-                    'name'=>'Ben',
-                    'hand'=>array('H1',"H2","HQ","HK"),
-                    'score'=>0
-                    ),
-                array(
-                    'name'=>'Abraham',
-                    'hand'=>array('H1',"H2","HQ","HK"),
-                    'score'=>0
-                    ),
-                array(
-                    'name'=>'A Cat',
-                    'hand'=>array('H1',"H2","HQ","HK"),
-                    'score'=>0
-                    ),
-                array(
-                    'name'=>'Breenstar The Dolphin Boy',
-                    'hand'=>array('H1',"H2","HQ","HK"),
-                    'score'=>0
-                    )),
-            'winners'=>array(1,2,3,4)
-            );   
-        return $state;
-          */  
+    }
+    function getCardImagePath($card){
+        $path="";
+        switch(substr($card,0,1)){
+            case "C":
+                $suit="clubs";
+                break;
+            case "D":
+                $suit="diamonds";
+                break;
+            case "H":
+                $suit="hearts";
+                break;
+            case "S":
+                $suit="spades";
+                break;
+            default:
+                return "img/cards/card_back.png";
+        }
+        $number=substr($card,1);
+        return "img/cards/$suit/$number.png";
+    }
+    function displayPlayer($player,$isWinner){
+        if($isWinner){
+            echo "<div class='TableRow Winner'>"; 
+        }else{
+            echo "<div class='TableRow'>";
+        }
+        echo 
+            "<div class='TableItem NameItem' column-header='Name'>".
+            $player['name'].
+            "</div>";
+            
+            $pad=6-sizeof($player['hand']);
+            
+        foreach($player['hand'] as $card){
+            echo 
+                "<div class='TableItem CardItem'>".
+                "<img src='".getCardImagePath($card)."'/>".
+                "</div>";
+        }
+        for($pad;$pad>=0;$pad--){
+            "<div class='TableItem CardItem'>".
+                "<img src='"."img/cards/card_back.png"."'/>".
+                "</div>";
+        }
+        
+        echo 
+            "<div class='TableItem ScoreItem' column-header='Name'>".
+            $player['score'].
+            "</div>";
+        
+        echo "</div>";
     }
     function display(){
         $state=adapt(run());
+        echo "<div class='Table'>";
         for($playerIdx=0;$playerIdx<4;$playerIdx++){
             
             $player=$state['players'][$playerIdx];
@@ -45,21 +73,9 @@
                 }
             }
             
-            if($isWinner){
-                echo '<tr class="player winner">';
-            }else{
-               echo '<tr class="player">'; 
-            }
-            
-            echo '<td class="playerPhoto item"></td>';
-            echo '<td class="playerName item">'.$player['name']."</td>";
-            
-            foreach($player['hand'] as $card){
-                    echo "<td class='card item'>$card</td>";
-            }
-            
-            echo '</tr>';
+            displayPlayer($player,$isWinner);
             
         }
+        echo "</div>";
     }
 ?>
